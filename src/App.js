272b855df9eb108eb0aed0c4a2a38681e3a6ac94
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-14 11:06:44 -0500

    Create item menu Component basic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
+
 import './App.css';
 import { Route } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import RegisterPage from './Pages/RegisterPage/RegisterPage';
 
+
 function App() {
 
-  const location = useLocation()
 
   return (
     <div className="App">
-      {location !== "/login" && (<Route path='/' />)}
       <Route path='/login' component={LoginPage} />
-      <Route exact path='/dashboard'/>
-      <Route exact path='/create_menu'/>
-      <Route exact path='/create_recipe'/>
-      <Route exact path='/register' component={RegisterPage}/>
+      <Route exact path='/dashboard' />
+      <Route exact path='/create_menu' />
+      <Route exact path='/create_recipe' />
+      <Route exact path='/register' component={RegisterPage} />
       <Route exact path='/create_ingredients' />
       <Route exact path='/profile' />
       <Route exact path='/store' />
