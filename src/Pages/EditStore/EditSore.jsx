950679cb50e40641174647527638b3453efedafd
ninak8 <103591333+ninak8@users.noreply.github.com>
2023-02-14 20:59:25 -0300

    se crea el componente "Edit Store"

---
+++ src/Pages/EditStore/EditSore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditStore from "../../components/CreateEditStore/CreateEditStore";
+
+const CreateEditStore = (props) => {
+  return (
+    <div>
+      "Navbar"
+      <EditStore />
+    </div>
+  );
+};
+export default CreateEditStore;
