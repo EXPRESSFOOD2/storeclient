28eb5aae67019dc788b3b4597ce79ca3869f7ade
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-15 18:54:45 -0500

    adding form controller

--- src/pages/createItemMenu/CreateItemMenu.jsx
+++ src/pages/createItemMenu/CreateItemMenu.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import Form from "../../components/createItemMenu/Form";
 import NavBar from "../../Shared/NavBar/NavBar";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function CreateItemMenu() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      price: "",
+      stock: "",
+      recomendado: false,
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("El nombre es requerido"),
+      description: Yup.string().required("La descripción es requerida"),
+      price: Yup.number()
+        .min(1, "El precio debe ser minimo 1")
+        .required("Precio requerido"),
+      stock: Yup.number()
+        .min(1, "El stock debe ser minimo 1")
+        .required("Stock requerido"),
+    }),
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div>
-      <NavBar/>
-      <Form />
+      <NavBar />
+      <Form formik={formik} />
     </div>
   );
 }
