520bdbb83516236987c779041d96fddde1683d65
ninak8 <103591333+ninak8@users.noreply.github.com>
2023-02-16 03:57:22 -0300

    se realizaron las actions "createUser" y "deleteIngredient"
    también a parte de las actions se agregó  un formik al componente "register" y una pequeña validación de prueba con Yup

--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import Register from "../../components/Register/Register";
 import NavBar from "../../Shared/NavBar/NavBar";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-const RegisterPage = () => {
-    return (
-        <div>
-            <NavBar/>
-            <Register />
-        </div>
-    );
+const RegisterPage = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+      username: "",
+      phone: "",
+      password: "",
+      password_question: "",
+      password_answer: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("El nombre es requerido"),
+      lastname: Yup.string().required("El apellido es requerido"),
+      email: Yup.string()
+        .email("Email invalido")
+        .required("El email es requerido"),
+      username: Yup.string().required("El Nombre de Usuario es requerido"),
+      phone: Yup.number("Deben se ser números").required("Telefono requerido"),
+      password: Yup.string().required("debe contener mas de 10 digitos"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <NavBar />
+      <Register formik={formik} />
+    </div>
+  );
 };
 
 export default RegisterPage;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import styles from "./Register.module.css";
 import Title from "../../Shared/Title/Title";
 import img from "./image/image-1.jpg";
+import styles from "./Register.module.css";
 
-const Register = () => {
+const Register = ({ formik }) => {
+  console.log(formik.values);
   return (
     <div className={styles.register}>
       <Title data="Regístrate" />
       <div className={styles.container}>
-        <form className={styles.formBox}>
+        <form className={styles.formBox} onSubmit={formik.handleSubmit}>
           <div className={styles.formLeft}>
             <img src={img} alt="" />
             <svg
@@ -25,39 +26,136 @@ const Register = () => {
           <div className={styles.formRight}>
             <div className={styles.inputs}>
               <label htmlFor="name">Nombre</label>
-              <input type="text" placeholder="nombre" name="name" />
+              <input
+                type="text"
+                placeholder="nombre"
+                className={formik.errors.name ? styles.errorInput : styles.ok}
+                id="name"
+                name="name"
+                {...formik.getFieldProps("name")}
+              />
+              {formik.touched.name && formik.errors.name ? (
+                <label className={styles.errorText}>{formik.errors.name}</label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <label htmlFor="lastname">Apellido</label>
-              <input type="text" placeholder="apellido" name="lastname" />
+              <input
+                type="text"
+                placeholder="apellido"
+                className={
+                  formik.errors.lastname ? styles.errorInput : styles.ok
+                }
+                id="lastname"
+                name="lastname"
+                {...formik.getFieldProps("lastname")}
+              />
+              {formik.touched.lastname && formik.errors.lastname ? (
+                <label className={styles.errorText}>
+                  {formik.errors.lastname}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <label htmlFor="username">Nombre de usuario</label>
               <input
                 type="text"
                 placeholder="nombre de usuario"
+                className={
+                  formik.errors.username ? styles.errorInput : styles.ok
+                }
+                id="username"
                 name="username"
+                {...formik.getFieldProps("username")}
               />
+              {formik.touched.username && formik.errors.username ? (
+                <label className={styles.errorText}>
+                  {formik.errors.username}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <label htmlFor="phone">Telefono</label>
-              <input type="text" placeholder="telefono" name="phone" />
+              <input
+                type="text"
+                placeholder="telefono"
+                className={formik.errors.phone ? styles.errorInput : styles.ok}
+                id="phone"
+                name="phone"
+                {...formik.getFieldProps("phone")}
+              />
+              {formik.touched.phone && formik.errors.phone ? (
+                <label className={styles.errorText}>
+                  {formik.errors.phone}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
-              <label htmlFor="mail">Email</label>
-              <input type="text" placeholder="correo" name="mail" />
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                placeholder="correo"
+                className={formik.errors.email ? styles.errorInput : styles.ok}
+                id="email"
+                name="email"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <label className={styles.errorText}>
+                  {formik.errors.email}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <label htmlFor="password">Contraseña</label>
-              <input type="text" placeholder="contraseña" name="password" />
+              <input
+                type="password"
+                placeholder="contraseña"
+                className={
+                  formik.errors.password ? styles.errorInput : styles.ok
+                }
+                id="password"
+                name="password"
+                {...formik.getFieldProps("password")}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <label className={styles.errorText}>
+                  {formik.errors.password}
+                </label>
+              ) : null}
             </div>
+            {/*//? ******************************************** */}
             <div className={styles.inputs}>
               <label htmlFor="">Password_question</label>
-              <input type="text" placeholder="contraseña" />
+              <input
+                type="text"
+                placeholder="contraseña"
+                id="password_question"
+                name="password_question"
+                {...formik.getFieldProps("password_question")}
+              />
+              {formik.touched.password_question &&
+              formik.errors.password_question ? (
+                <label className={styles.errorText}>
+                  {formik.errors.password_question}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <label htmlFor="">password_answer</label>
-              <input type="text" placeholder="contraseña" />
+              <input
+                type="text"
+                placeholder="contraseña"
+                id="password_answer"
+                name="password_answer"
+                {...formik.getFieldProps("password_answer")}
+              />
+              {formik.touched.password_answer &&
+              formik.errors.password_answer ? (
+                <label className={styles.errorText}>
+                  {formik.errors.password_answer}
+                </label>
+              ) : null}
             </div>
             <div className={styles.inputs}>
               <button>Registrar</button>
