8198c25b058c8854f267e3466e59876d6911efa5
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-02-16 12:41:31 -0500

    Se agrego validacion con yup al loginPage

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import Title from "../../Shared/Title/Title";
-import styles from "./Login.module.css";
+import style from "./Login.module.css";
 
-const Login = () => {
-    return (
-        <div className={styles.login}>
-            <Title data="Bienvenido" />
-            <form className={styles.data}>
-                <input type="text" placeholder="correo" className={styles.inputData} />
-                <input type="text" placeholder="contraseña" className={styles.inputData} />
-                <span>Recuperar mi contraseña</span>
-                <button>Iniciar sesión</button>
-                <div className={styles.checkbox}>
-                    <input name="remember" type="checkbox" value="Remember me" id="remember" />
-                    <label htmlFor="remember">Recordarme</label>
-                </div>
-            </form>
+export default function Login({ formik }) {
+  return (
+    <div className={style.login}>
+      <Title data="Bienvenido" />
+      <form className={style.data} onSubmit={formik.handleSubmit}>
+        <div className={style.col}>
+          <input
+            type="text"
+            name="correo"
+            placeholder="correo"
+            className={formik.errors.correo ? style.errorInput : ""}
+            onChange={formik.handleChange}
+            value={formik.values.correo}
+          />
+           {formik.errors.correo && (
+            <label className={style.errorText}>
+              {formik.errors.correo}
+            </label>
+          )}
         </div>
-    );
-};
-
-export default Login;
+        <div className={style.col}>
+          <input
+            name="contraseña"
+            type="text"
+            placeholder="contraseña"
+            className={formik.errors.contraseña ? style.errorInput : ""}
+            onChange={formik.handleChange}
+            value={formik.values.contraseña}
+          />
+          {formik.errors.contraseña && (
+            <label className={style.errorText}>
+              {formik.errors.contraseña}
+            </label>
+          )}
+        </div>
+        <span>Recuperar mi contraseña</span>
+        <button type="submit">Iniciar sesión</button>
+        <div className={style.checkbox}>
+          <input
+            name="remember"
+            type="checkbox"
+            value="Remember me"
+            id="remember"
+          />
+          <label htmlFor="remember">Recordarme</label>
+        </div>
+      </form>
+    </div>
+  );
+}
