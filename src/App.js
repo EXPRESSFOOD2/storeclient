f143415d78d0d1b752b4eae8e67dd1aa30af123e
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-20 15:01:14 -0500

    adding basic alert dynamic

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Ingredients from "./Pages/Ingredients/Ingredients";
 import GetRecipe from "./Pages/Recipe/Get/GetRecipe"
 
 import UpdateIngredient from "./Pages/UpdateIngredient/UpdateIngredient";
+import Alert from "./Shared/Alert/Alert";
 
 //Se establece la base de axios pensando en el deploy
 axios.defaults.baseURL = "http://localhost:3001/"
@@ -26,8 +27,8 @@ function App() {
       <Route exact path="/menu/create" component={CreateItemMenu} />
       <Route exact path="/menu/update/:id" component={CreateItemMenu} />
       { }
-      <Route exact path="/recipe/create"/>
-      <Route exact path="/get/recipe" component={GetRecipe}/> 
+      <Route exact path="/recipe/create" />
+      <Route exact path="/get/recipe" component={GetRecipe} />
       <Route exact path="/register" component={RegisterPage} />
       { }
       <Route exact path="/ingredient" component={Ingredients} />
@@ -38,6 +39,7 @@ function App() {
       <Route exact path="/store" />
       <Route exact path="/orders" />
       <Route exact path="/store/update" component={EditStore} />
+      <Alert />
     </div>
   );
 }
