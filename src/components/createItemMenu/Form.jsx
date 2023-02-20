cb96a12b06891e64ef456c2eeae9e93c661123f7
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-20 10:52:40 -0500

    Revert "change message in carousel"
    This reverts commit eff846b6866594d79c7e6b16792d2068c4396b5c.

--- src/components/createItemMenu/Form.jsx
+++ src/components/createItemMenu/Form.jsx
@@ -41,7 +41,6 @@ export default function Form({ path, menu, ingredientes }) {
     };
   };
 
-  // input type = file
   const imgDefault =
     "https://c8.alamy.com/compes/her4w9/comida-tradicional-mexicana-de-fondo-con-el-burrito-croquis-dibujados-a-mano-ilustracion-vectorial-mexico-cocina-vintage-banner-her4w9.jpg";
 
@@ -88,8 +87,8 @@ export default function Form({ path, menu, ingredientes }) {
       if (path === "update") {
         dispatch(updateMenu({ ...menuMapData, id: menu.id }));
       } else {
-        // dispatch(createMenu(menuMapData));
-        console.log(urlImage);
+        dispatch(createMenu(menuMapData));
+        console.log(menuMapData);
       }
     },
   });
