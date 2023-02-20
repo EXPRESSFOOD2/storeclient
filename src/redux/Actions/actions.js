a53f18b6315793c04b1984a2dc9820b7bc279e67
ninak8 <103591333+ninak8@users.noreply.github.com>
2023-02-19 21:41:09 -0300

    se crea el input type file en "Register"
    se corrigió una importación en las Actions

--- src/redux/Actions/actions.js
+++ src/redux/Actions/actions.js
@@ -1,6 +1,13 @@
 import axios from "axios";
-import { GET_MENU, ERROR, GET_ALL_INGREDIENTS, FILTER, CREATE_MENU, UPDATE_MENU } from "./types";
-
+import {
+  GET_MENU,
+  ERROR,
+  GET_ALL_INGREDIENTS,
+  FILTER,
+  CREATE_MENU,
+  UPDATE_MENU,
+  GET_INGREDIENT_ID,
+} from "./types";
 
 export const getMenu = () => {
   return async function (dispatch) {
@@ -25,8 +32,6 @@ export const getAllIngredients = () => {
       dispatch({ type: ERROR, payload: error.response.data.error });
     }
   };
-
-
 };
 
 export const getIngredientById = (id) => {
@@ -49,27 +54,27 @@ export const updateIngredient = async (data) => {
 export const createMenu = (data) => {
   return async function (dispatch) {
     try {
-      const newMenu = (await axios.post("/menu/create", data));
+      const newMenu = await axios.post("/menu/create", data);
       dispatch({ type: CREATE_MENU, payload: newMenu });
     } catch (error) {
       dispatch({ type: ERROR, payload: error.response.data.error });
     }
   };
-}
+};
 
 export const updateMenu = (data) => {
   return async function (dispatch) {
     try {
-      const newMenu = (await axios.patch("/menu/update", data));
+      const newMenu = await axios.patch("/menu/update", data);
       dispatch({ type: UPDATE_MENU, payload: newMenu });
     } catch (error) {
       dispatch({ type: ERROR, payload: error.response.data.error });
     }
   };
-}
+};
 
 export const filter = () => (dispatch) => {
-  dispatch({ type: FILTER })
+  dispatch({ type: FILTER });
 };
 
 export const createUser = (user) => {
@@ -87,7 +92,6 @@ export const deleteIngredient = (id) => {
     .catch((err) => console.error(err));
   // después veré que hacer con la respuesta o el error por el momento la consologeo
 };
-
 
 export const resetError = () => {
   //Funcion para resetear el estado de redux error en false, despues de un error
