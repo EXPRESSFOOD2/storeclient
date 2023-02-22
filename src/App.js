5543ebe31f0fe483d701cc192cf2c6436bb9ff01
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-02-21 22:08:09 -0500

    Se guarda la sesion en el localStorage y protege rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import axios from 'axios';
+import axios from "axios";
 import { Route, useHistory } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import RegisterPage from "./Pages/RegisterPage/RegisterPage";
@@ -8,27 +8,39 @@ import EditStore from "./Pages/EditStore/EditSore";
 import Menu from "./Pages/Menu/Menu";
 import Ingredients from "./Pages/Ingredients/Ingredients";
 
-import GetRecipe from "./Pages/Recipe/Get/GetRecipe"
+import GetRecipe from "./Pages/Recipe/Get/GetRecipe";
 
 import UpdateIngredient from "./Pages/UpdateIngredient/UpdateIngredient";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { changeLoginStatus } from "./redux/Actions/actions";
 
 //Se establece la base de axios pensando en el deploy
-axios.defaults.baseURL = "http://localhost:3001/"
+axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
   const loginStatus = useSelector((state) => state.loginStatus);
+  const dispatch = useDispatch();
   const history = useHistory();
+  const userLogin = window.localStorage.getItem("userLogin");
+
+  // useEffect(() => {
+  //   if (!loginStatus) {
+  //     history.push("/");
+  //   }
+  // }, [loginStatus]);  
+
+  //Se cambio la verificacion del login a estadoLocal. 
 
   useEffect(() => {
-    if (!loginStatus) {
+    if (userLogin === "false" || !userLogin) {
       history.push("/");
+    } else {
+      dispatch(changeLoginStatus(true));
+      history.push("/menu");
     }
-  }, [loginStatus]);
+  }, [userLogin]);
   return (
-    
     <div className="App">
       <Route exact path="/" component={LoginPage} />
       <Route exact path="/dashboard" />
@@ -36,20 +48,20 @@ function App() {
       <Route exact path="/menu" component={Menu} />
       <Route exact path="/menu/create" component={CreateItemMenu} />
       <Route exact path="/menu/update/:id" component={CreateItemMenu} />
-      { }
+      {}
       <Route exact path="/recipe/create" />
       <Route exact path="/get/recipe" component={GetRecipe} />
       <Route exact path="/register" component={RegisterPage} />
-      { }
+      {}
       <Route exact path="/ingredient" component={Ingredients} />
       <Route exact path="/ingredient/create" component={Ingredients} />
-      { }
+      {}
       <Route exact path="/profile" />
       <Route exact path="/store" />
       <Route exact path="/orders" />
       <Route exact path="/store/update" component={EditStore} />
-      <div id="alert" className="alert" ></div>
-    </div >
+      <div id="alert" className="alert"></div>
+    </div>
   );
 }
 
