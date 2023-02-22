1286e825769a8d489d03069a5838ba4073de1936
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-02-22 15:02:13 -0500

    Adding name and img at nav after loggin

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -8,22 +8,27 @@ import { changeLoginStatus } from "../../redux/Actions/actions";
 
 const NavBar = () => {
   const location = useLocation().pathname.split("/").at(1);
-  const dispatch = useDispatch()
-  
-  function handleResetUserLogin() { 
+  const dispatch = useDispatch();
+  const userData = JSON.parse(window.localStorage.getItem("userData"));
+
+  function handleResetUserLogin() {
     try {
       window.localStorage.setItem("userLogin", "false");
+      window.localStorage.removeItem("userData");
     } catch (error) {
       console.error(error);
-    }  
-    dispatch(changeLoginStatus(false))
-
+    }
+    dispatch(changeLoginStatus(false));
   }
 
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
         <img src={img} alt="" className={styles.logo} />
+      </div>
+      <div className={styles.userContainer}>
+        {userData.img && <img src={userData.img} alt="" />}
+        {userData.name && <span>{userData.name}</span>}
       </div>
       <div className={styles.buttons}>
         <Link to="/store/update">
@@ -44,9 +49,9 @@ const NavBar = () => {
         </Link>
 
         <Link to="get/recipe">
-        <button className={location === "recipe" ? styles.currentPag : ""}>
-          Receta
-        </button>
+          <button className={location === "recipe" ? styles.currentPag : ""}>
+            Receta
+          </button>
         </Link>
 
         <Link to="/ingredient">
@@ -57,7 +62,7 @@ const NavBar = () => {
           </button>
         </Link>
         <Link to="/">
-          <button onClick={()=>handleResetUserLogin()}>Salir</button>
+          <button onClick={() => handleResetUserLogin()}>Salir</button>
         </Link>
       </div>
     </div>
