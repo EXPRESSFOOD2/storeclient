4a8757d598518c5037c9025a14dc76525c63a12a
Davidvn95 <davidvergaraok@gmail.com>
2023-03-04 18:01:45 -0500

    Implementando delete

--- src/redux/Reducer/reducer.js
+++ src/redux/Reducer/reducer.js
@@ -16,7 +16,9 @@ import {
   FILTER_BY_TAG,
   ORDER_BY_RECOMMENDATION,
   SORT_BY_ACTIVITY,
-  ORDER_BY_QUANTITY
+  ORDER_BY_QUANTITY,
+  DELETE_INGREDIENT,
+  DELETE_RECIPE
 } from '../Actions/types'
 import { createRoot } from 'react-dom/client'
 
@@ -72,13 +74,23 @@ const rootReducer = (state = initialState, action) => {
       return { ...state, ingredientDetail: action.payload }
     case CREATE_INGREDIENTS:
       return { ...state, ingredients: [...state.ingredients, ...action.payload] }
+    case DELETE_INGREDIENT:
+      const ingredients = state.ingredients?.filter(element => element.id !== action.payload)
+      return {
+        ...state, ingredients:[...ingredients],
+      }
     case ERROR:
       const root = createRoot(document.getElementById('alert'))
       root.render(<Alert title="Error" message={action.payload} type="danger" />)
       return { ...state, errors: action.payload }
       
       case GET_ROLES:
-        return { ...state, roles: action.payload }
+      return { ...state, roles: action.payload }
+    case DELETE_RECIPE:
+      const recipes = state.render_receta?.map(element => element.id !== action.payload)
+      return {
+        ...state, render_receta: [...recipes]
+      }
         
               case GET_RECETA:
               return { ...state, recetas: action.payload, render_receta: [...action.payload] };
